e90a84e816ab245280708a7d4433c9ad7a03e59a
serkancam <sekocam@gmail.com>
2022-05-25 19:35:57 +0300

    guncell

--- snake_game.py
+++ snake_game.py
@@ -1,98 +1,157 @@
-from random import random
-import cv2
+import math
+import random
 import mediapipe as mp
 import math
-import time
+import cv2
 import numpy as np
-# import tensorflow as tf
-
-# # Eğer tensorflow gpu da çalışıyorsa onu engelleyip cpu ya almak için
-# tf.config.set_visible_devices([], 'GPU')
 
 
-class handDetector():
-    def __init__(self, mode=False, maxHands=1, detectionCon=0.5, trackCon=0.5):
-        # tf.config.set_visible_devices([], 'GPU')
+class HandDetector:
+    """
+    Finds Hands using the mediapipe library. Exports the landmarks
+    in pixel format. Adds extra functionalities like finding how
+    many fingers are up or the distance between two fingers. Also
+    provides bounding box info of the hand found.
+    """
+
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, minTrackCon=0.5):
+        """
+        :param mode: In static mode, detection is done on each image: slower
+        :param maxHands: Maximum number of hands to detect
+        :param detectionCon: Minimum Detection Confidence Threshold
+        :param minTrackCon: Minimum Tracking Confidence Threshold
+        """
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
-        self.trackCon = trackCon
+        self.minTrackCon = minTrackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(static_image_mode=self.mode, 
-                                        max_num_hands=self.maxHands,
-                                        min_detection_confidence=self.detectionCon, 
-                                        min_tracking_confidence=self.trackCon)
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode, max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectionCon,
+                                        min_tracking_confidence=self.minTrackCon)
         self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+        self.fingers = []
+        self.lmList = []
 
-    def findHands(self, img, draw=True):
+    def findHands(self, img, draw=True, flipType=True):
+        """
+        Finds hands in a BGR image.
+        :param img: Image to find the hands in.
+        :param draw: Flag to draw the output on the image.
+        :return: Image with or without drawings
+        """
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
-
+        allHands = []
+        h, w, c = img.shape
         if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-                if draw:
-                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
-        return img
+            for handType, handLms in zip(self.results.multi_handedness, self.results.multi_hand_landmarks):
+                myHand = {}
+                ## lmList
+                mylmList = []
+                xList = []
+                yList = []
+                for id, lm in enumerate(handLms.landmark):
+                    px, py, pz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
+                    mylmList.append([px, py, pz])
+                    xList.append(px)
+                    yList.append(py)
 
-    def findPosition(self, img, handNo=0, draw=True):
-        lmlist = []
+                ## bbox
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+                boxW, boxH = xmax - xmin, ymax - ymin
+                bbox = xmin, ymin, boxW, boxH
+                cx, cy = bbox[0] + (bbox[2] // 2), \
+                         bbox[1] + (bbox[3] // 2)
+
+                myHand["lmList"] = mylmList
+                myHand["bbox"] = bbox
+                myHand["center"] = (cx, cy)
+
+                if flipType:
+                    if handType.classification[0].label == "Right":
+                        myHand["type"] = "Left"
+                    else:
+                        myHand["type"] = "Right"
+                else:
+                    myHand["type"] = handType.classification[0].label
+                allHands.append(myHand)
+
+                ## draw
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+                    cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+                                  (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
+                                  (255, 0, 255), 2)
+                    cv2.putText(img, myHand["type"], (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
+                                2, (255, 0, 255), 2)
+        if draw:
+            return allHands, img
+        else:
+            return allHands
+
+    def fingersUp(self, myHand):
+        """
+        Finds how many fingers are open and returns in a list.
+        Considers left and right hands separately
+        :return: List of which fingers are up
+        """
+        myHandType = myHand["type"]
+        myLmList = myHand["lmList"]
         if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                lmlist.append([id, cx, cy])
-                if draw:
-                    cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
-        return lmlist
+            fingers = []
+            # Thumb
+            if myHandType == "Right":
+                if myLmList[self.tipIds[0]][0] > myLmList[self.tipIds[0] - 1][0]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            else:
+                if myLmList[self.tipIds[0]][0] < myLmList[self.tipIds[0] - 1][0]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
 
-class SnakeGame():
-    def __init__(self) -> None:
-        self.points = [] # list of points
-        self.lengths=[]# list of lengths
-        self.currentLength = 0 # total length of snake
-        self.allowedLength = 150 # total allowed length 
-        self.previousHead = (0,0) # previous head position
-        # self.pathFood="/media/serkancam/yedek/Calismalar/python/okulCalismalar/oyg_calisma/opencv/images/elma.jpg"
-        # self.imgFood=cv2.imread(self.pathFood,cv2.IMREAD_UNCHANGED)
-        # self.hFood,self.wFood,self.cFood=self.imgFood.shape
-        # self.foodPoint=(0,0)
-        # self.randomFoodLocation()
-    
-    def randomFoodLocation(self):
-        self.foodPoint=random.randint(100,1000),random.randint(100,600)
-    
-    def update(self,imgMain, currentHead):
-        
-        px,py = self.previousHead
-        cx,cy=currentHead        
-        self.points.append((cx,cy))
-        distance = math.hypot(cx-px,cy-py)#((cx-px)**2 + (cy-py)**2)**0.5
-        self.lengths.append(distance)
-        self.currentLength += distance
-        self.previousHead=currentHead#cx,cy
-        #length reducion    
-        if self.currentLength>self.allowedLength:
-            for i,length in enumerate(self.lengths):
-                self.currentLength -= length
-                self.points.pop(i)# del self.points[i]
-                self.lengths.pop(i)# del self.lengths[i]
-                if self.currentLength<self.allowedLength:
-                    break 
-               
-        #draw snake
-        if self.points:
-            for i,point in enumerate(self.points):
-                if i!=0:
-                    cv2.line(imgMain,self.points[i-1],self.points[i],(0,0,255),20)
-            cv2.circle(imgMain,self.points[-1],20,(200,0,200),cv2.FILLED)
-        #draw food
-        
-        return imgMain
+            # 4 Fingers
+            for id in range(1, 5):
+                if myLmList[self.tipIds[id]][1] < myLmList[self.tipIds[id] - 2][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+        return fingers
 
-def overlayPNG(self,imgBack, imgFront, pos=[0, 0]):
+    def findDistance(self, p1, p2, img=None):
+        """
+        Find the distance between two landmarks based on their
+        index numbers.
+        :param p1: Point1
+        :param p2: Point2
+        :param img: Image to draw on.
+        :param draw: Flag to draw the output on the image.
+        :return: Distance between the points
+                 Image with output drawn
+                 Line information
+        """
+
+        x1, y1 = p1
+        x2, y2 = p2
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        length = math.hypot(x2 - x1, y2 - y1)
+        info = (x1, y1, x2, y2, cx, cy)
+        if img is not None:
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            return length, info, img
+        else:
+            return length, info
+
+def overlayPNG(imgBack, imgFront, pos=[0, 0]):
     hf, wf, cf = imgFront.shape
     hb, wb, cb = imgBack.shape
     *_, mask = cv2.split(imgFront)
@@ -112,34 +171,142 @@ def overlayPNG(self,imgBack, imgFront, pos=[0, 0]):
 
     return imgBack
 
-pTime = 0
-cTime = 0
+def putTextRect(img, text, pos, scale=3, thickness=3, colorT=(255, 255, 255),
+                colorR=(255, 0, 255), font=cv2.FONT_HERSHEY_PLAIN,
+                offset=10, border=None, colorB=(0, 255, 0)):
+    """
+    Creates Text with Rectangle Background
+    :param img: Image to put text rect on
+    :param text: Text inside the rect
+    :param pos: Starting position of the rect x1,y1
+    :param scale: Scale of the text
+    :param thickness: Thickness of the text
+    :param colorT: Color of the Text
+    :param colorR: Color of the Rectangle
+    :param font: Font used. Must be cv2.FONT....
+    :param offset: Clearance around the text
+    :param border: Outline around the rect
+    :param colorB: Color of the outline
+    :return: image, rect (x1,y1,x2,y2)
+    """
+    ox, oy = pos
+    (w, h), _ = cv2.getTextSize(text, font, scale, thickness)
+
+    x1, y1, x2, y2 = ox - offset, oy + offset, ox + w + offset, oy - h - offset
+
+    cv2.rectangle(img, (x1, y1), (x2, y2), colorR, cv2.FILLED)
+    if border is not None:
+        cv2.rectangle(img, (x1, y1), (x2, y2), colorB, border)
+    cv2.putText(img, text, (ox, oy), font, scale, colorT, thickness)
+
+    return img, [x1, y2, x2, y1]
+
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-detector = handDetector()
+cap.set(3, 1280)
+cap.set(4, 720)
 
-game=SnakeGame()
+detector = HandDetector(detectionCon=0.8, maxHands=1)
 
+
+class SnakeGameClass:
+    def __init__(self, pathFood):
+        self.points = []  # all points of the snake
+        self.lengths = []  # distance between each point
+        self.currentLength = 0  # total length of the snake
+        self.allowedLength = 150  # total allowed Length
+        self.previousHead = 0, 0  # previous head point
+
+        self.imgFood = cv2.imread(pathFood, cv2.IMREAD_UNCHANGED)
+        self.hFood, self.wFood, _ = self.imgFood.shape
+        self.foodPoint = 0, 0
+        self.randomFoodLocation()
+
+        self.score = 0
+        self.gameOver = False
+
+    def randomFoodLocation(self):
+        self.foodPoint = random.randint(300, 1000), random.randint(100, 300)
+
+    def update(self, imgMain, currentHead):
+
+        if self.gameOver:
+            putTextRect(imgMain, "Game Over", [300, 400],
+                               scale=7, thickness=5, offset=20)
+            putTextRect(imgMain, f'Your Score: {self.score}', [300, 550],
+                               scale=7, thickness=5, offset=20)
+        else:
+            px, py = self.previousHead
+            cx, cy = currentHead
+
+            self.points.append([cx, cy])
+            distance = math.hypot(cx - px, cy - py)
+            self.lengths.append(distance)
+            self.currentLength += distance
+            self.previousHead = cx, cy
+
+            # Length Reduction
+            if self.currentLength > self.allowedLength:
+                for i, length in enumerate(self.lengths):
+                    self.currentLength -= length
+                    self.lengths.pop(i)
+                    self.points.pop(i)
+                    if self.currentLength < self.allowedLength:
+                        break
+
+            # Check if snake ate the Food
+            rx, ry = self.foodPoint
+            if rx - self.wFood // 2 < cx < rx + self.wFood // 2 and \
+                    ry - self.hFood // 2 < cy < ry + self.hFood // 2:
+                self.randomFoodLocation()
+                self.allowedLength += 50
+                self.score += 1
+                print(self.score)
+
+            # Draw Snake
+            if self.points:
+                for i, point in enumerate(self.points):
+                    if i != 0:
+                        cv2.line(imgMain, self.points[i - 1], self.points[i], (0, 0, 255), 20)
+                cv2.circle(imgMain, self.points[-1], 20, (0, 255, 0), cv2.FILLED)
+
+            # Draw Food
+            imgMain = overlayPNG(imgMain, self.imgFood,
+                                        (rx - self.wFood // 2, ry - self.hFood // 2))
+
+            putTextRect(imgMain, f'Score: {self.score}', [50, 80],
+                               scale=3, thickness=3, offset=10)
+
+            # Check for Collision
+            pts = np.array(self.points[:-2], np.int32)
+            pts = pts.reshape((-1, 1, 2))
+            cv2.polylines(imgMain, [pts], False, (0, 255, 0), 3)
+            minDist = cv2.pointPolygonTest(pts, (cx, cy), True)
+
+            if -1 <= minDist <= 1:
+                print("Çarptı")
+                self.gameOver = True
+                self.points = []  # all points of the snake
+                self.lengths = []  # distance between each point
+                self.currentLength = 0  # total length of the snake
+                self.allowedLength = 150  # total allowed Length
+                self.previousHead = 0, 0  # previous head point
+                self.randomFoodLocation()
+
+        return imgMain
+
+pathFood=os
+game = SnakeGameClass("Donut.png")
 
 while True:
     success, img = cap.read()
-    img=cv2.flip(img,1)
-    img = detector.findHands(img,draw=False)
-    lmlist = detector.findPosition(img=img,draw=False)
-    if len(lmlist) != 0:  
-       pointIndex=tuple(lmlist[8][1:])
-       img=game.update(imgMain=img,currentHead=pointIndex)
+    img = cv2.flip(img, 1)
+    hands, img = detector.findHands(img, flipType=False)
 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-
-    cv2.putText(img, str(int(fps)), (10, 70),
-                cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
+    if hands:
+        lmList = hands[0]['lmList']
+        pointIndex = lmList[8][0:2]
+        img = game.update(img, pointIndex)
     cv2.imshow("Image", img)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-cv2.destroyAllWindows()
+    key = cv2.waitKey(1)
+    if key == ord('r'):
+        game.gameOver = False
